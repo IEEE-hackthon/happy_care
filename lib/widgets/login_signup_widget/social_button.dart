35d816bc64f046eee420db08b1819e895963216c
Mohamed Elsayed <mohammed.sayed201638@gmail.com>
2024-08-01 05:01:11 +0300

    refactor social_button.dart

--- lib/widgets/login_signup_widget/social_button.dart
+++ lib/widgets/login_signup_widget/social_button.dart
@@ -1,52 +1,53 @@
 import 'package:flutter/material.dart';
 
 class SocialButtons extends StatelessWidget {
-  final double iconSize;
-  final double spacing;
+  const SocialButtons({super.key});
 
-  const SocialButtons({
-    super.key,
-    required this.iconSize,
-    required this.spacing,
-  });
+  static const double buttonWidth = 64.0;
+  static const double buttonHeight = 56.0;
+  static const double iconSize = 24.0;
+  static const double spacing = 20.0;
+  static const double borderWidth = 1.0;
 
   @override
   Widget build(BuildContext context) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-            backgroundColor: Colors.blue[800],
-            padding: EdgeInsets.all(iconSize / 2),
+        Container(
+          width: buttonWidth,
+          height: buttonHeight,
+          decoration: BoxDecoration(
+            color: Colors.blue[800],
+            border: Border.all(color: Colors.blue[800]!, width: borderWidth),
+            borderRadius: BorderRadius.circular(8.0),
           ),
-          onPressed: () {},
-          child: Icon(
-            Icons.facebook,
+          child: IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.facebook, color: Colors.white),
+            tooltip: 'Facebook',
+            iconSize: iconSize,
             color: Colors.white,
-            size: iconSize,
           ),
         ),
-        SizedBox(width: spacing),
-        ElevatedButton(
-          style: ElevatedButton.styleFrom(
-            shape: RoundedRectangleBorder(
-              side: const BorderSide(
-                color: Color(0xff717171),
-              ),
-              borderRadius: BorderRadius.circular(8.0),
-            ),
-            backgroundColor: Colors.white,
-            padding: EdgeInsets.all(iconSize / 2),
+        const SizedBox(width: spacing),
+        Container(
+          width: buttonWidth,
+          height: buttonHeight,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: Colors.grey[400]!, width: borderWidth),
+            borderRadius: BorderRadius.circular(8.0),
           ),
-          onPressed: () {},
-          child: Image.network(
-            'https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png',
-            height: iconSize,
-            width: iconSize,
+          child: IconButton(
+            onPressed: () {},
+            icon: Image.network(
+              'https://cdn1.iconfinder.com/data/icons/google-s-logo/150/Google_Icons-09-512.png',
+              height: iconSize,
+              width: iconSize,
+            ),
+            tooltip: 'Google',
+            iconSize: iconSize,
           ),
         ),
       ],
